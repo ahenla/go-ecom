5acbc3e065bfba2dc9f8d550543c2a37a0961a79
Andres Henriquez <rymbell@gmail.com>
2024-07-20 19:16:54 +0900

    handling types, store and encripted password

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,18 +1,21 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/ahenla/go-ecom/service/auth"
 	"github.com/ahenla/go-ecom/types"
 	"github.com/ahenla/go-ecom/utils"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -32,4 +35,30 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// check if the user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("User with email %s already exists", payload.Email))
+		return
+	}
+
+	// If doesn't exist, create a new user
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
+
 }
