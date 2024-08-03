787a44a1c7ac16daf1e1870754f71d7226dd16b0
Andres Henriquez <rymbell@gmail.com>
2024-08-03 16:04:46 +0900

    created function for JWT handling and environment variable for expiration date

--- config/env.go
+++ config/env.go
@@ -3,17 +3,19 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	PublicHost string
-	Port       string
-	DBUser     string
-	DBPassword string
-	DBAdress   string
-	DBName     string
+	PublicHost             string
+	Port                   string
+	DBUser                 string
+	DBPassword             string
+	DBAdress               string
+	DBName                 string
+	JWTExpirationInSeconds int64
 }
 
 var Envs = InitConfig()
@@ -23,12 +25,13 @@ func InitConfig() Config {
 	godotenv.Load()
 
 	return Config{
-		PublicHost: getENV("PUBLIC_HOST", "http://localhost"),
-		Port:       getENV("PORT", "8080"),
-		DBUser:     getENV("DB_USER", "root"),
-		DBPassword: getENV("DB_PASSWORD", "C0ntr4s3n4-"),
-		DBAdress:   fmt.Sprintf("%s:%s", getENV("DB_HOST", "127.0.0.1"), getENV("DB_PORT", "3306")),
-		DBName:     getENV("DB_NAME", "ecomerce_go"),
+		PublicHost:             getENV("PUBLIC_HOST", "http://localhost"),
+		Port:                   getENV("PORT", "8080"),
+		DBUser:                 getENV("DB_USER", "root"),
+		DBPassword:             getENV("DB_PASSWORD", "C0ntr4s3n4-"),
+		DBAdress:               fmt.Sprintf("%s:%s", getENV("DB_HOST", "127.0.0.1"), getENV("DB_PORT", "3306")),
+		DBName:                 getENV("DB_NAME", "ecomerce_go"),
+		JWTExpirationInSeconds: getENVAsInt("JWT_EXP", 3600*24*7),
 	}
 }
 
@@ -37,5 +40,17 @@ func getENV(key, fallback string) string {
 		return value
 	}
 
+	return fallback
+}
+
+func getENVAsInt(key string, fallback int64) int64 {
+	if value, ok := os.LookupEnv(key); ok {
+		i, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return fallback
+		}
+
+		return i
+	}
 	return fallback
 }
