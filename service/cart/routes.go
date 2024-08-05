c6b32e8a29b14968240efbbe8b9771ca41575c2f
Andres Henriquez <rymbell@gmail.com>
2024-08-05 17:38:22 +0900

    created get products by id function in the product store

--- service/cart/routes.go
+++ service/cart/routes.go
@@ -1,18 +1,22 @@
 package cart
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/ahenla/go-ecom/types"
+	"github.com/ahenla/go-ecom/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
-	store types.OrderStore
+	store        types.OrderStore
+	productStore types.ProductStore
 }
 
-func NewHandler(store types.OrderStore) *Handler {
-	return &Handler{store: store}
+func NewHandler(store types.OrderStore, productStore types.ProductStore) *Handler {
+	return &Handler{store: store, productStore: productStore}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -20,5 +24,18 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 }
 
 func (h *Handler) handleCheckout(w http.ResponseWriter, r *http.Request) {
+	var cart types.CartCheckoutPayload
+	if err := utils.ParseJSON(r, &cart); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(cart); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteJSON(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	ps, err := h.productStore.GetProducts(ProductsIDs)
 
 }
--- types/types.go
+++ types/types.go
@@ -10,6 +10,7 @@ type UserStore interface {
 
 type ProductStore interface {
 	GetProducts() ([]Product, error)
+	GetProductByID(ps []int) ([]Product, error)
 }
 
 type OrderStore interface {
