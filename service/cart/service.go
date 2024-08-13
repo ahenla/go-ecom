f1331cd34636d1edd733d715c60f5344bcaeb054
Andres Henriquez <rymbell@gmail.com>
2024-08-13 19:22:35 +0900

    created signature for create order function and sub function to check availability of products in stock

--- service/cart/service.go
+++ service/cart/service.go
@@ -18,3 +18,38 @@ func getCartItemsIDs(items []types.CartItem) ([]int, error) {
 
 	return productIds, nil
 }
+
+func (h *Handler) createOrder(ps []types.Product, items []types.CartItem, userID int) (int, float64, error) {
+	productMap := make(map[int]types.Product)
+	for _, product := range ps {
+		productMap[product.ID] = product
+	}
+	// check if all the products are in stock
+	if err := checkIfCartIsInStock(items, productMap); err != nil {
+	}
+	// calculate the total price
+	// reduce the quantity of the products in the database
+	// create the order
+	// create order items
+
+	return 0, 0, nil
+}
+
+func checkIfCartIsInStock(cartItems []types.CartItem, products map[int]types.Product) error {
+	if len(cartItems) == 0 {
+		return fmt.Errorf("The cart is empty")
+	}
+
+	for _, item := range cartItems {
+		product, ok := products[item.ProductID]
+		if !ok {
+			return fmt.Errorf("product %d is not available in the store, please refresh your cart", item.ProductID)
+		}
+
+		if product.Quantity < item.Quantity {
+			return fmt.Errorf("product %s is not available in the requested quantity", product.Name)
+		}
+	}
+
+	return nil
+}
