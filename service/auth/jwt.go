024e5cb9193399cf86fb3b0e7e8545460edeb6da
Andres Henriquez <rymbell@gmail.com>
2024-08-15 18:45:46 +0900

    created helper function to validate JWT

--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -29,7 +30,10 @@ func CreateJWT(secret []byte, userID int) (string, error) {
 func WithJWTAuth(handlerFunc http.HandlerFunc, store types.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//get the token from the user's request
+		tokenString := getTokenFromRequest(r)
+
 		//validate the JWT
+		token, err := validateToken(tokenString)
 		//fetch the userID from the db (id from the token)
 		//set context "userID" to the user ID
 	}
@@ -43,4 +47,14 @@ func getTokenFromRequest(r *http.Request) string {
 	}
 
 	return ""
+}
+
+func validateToken(t string) (*jwt.Token, error) {
+	return jwt.Parse(t, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return []byte(config.Envs.JWTSecret), nil
+	})
 }
